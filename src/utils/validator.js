beb33cda21c90585dba13d109362985dea9b39d3
Bisrat Dereje <bdereje996@gmail.com>
2025-07-16 20:43:43 +0300

    strong validator

--- src/utils/validator.js
+++ src/utils/validator.js
@@ -3,7 +3,12 @@ const { body, validationResult } = require('express-validator');
 const signupValidationRules = () => [
   body('name').notEmpty().withMessage('Name is required'),
   body('email').isEmail().withMessage('Valid email is required'),
-  body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters'),
+  body('password')
+    .isLength({ min: 8 }).withMessage('Password must be at least 8 characters long')
+    .matches(/[A-Z]/).withMessage('Password must contain at least one uppercase letter')
+    .matches(/[a-z]/).withMessage('Password must contain at least one lowercase letter')
+    .matches(/[0-9]/).withMessage('Password must contain at least one number')
+    .matches(/[@$!%*?&]/).withMessage('Password must contain at least one special character (@$!%*?&)'),
   body('confirmPassword').custom((value, { req }) => {
     if (value !== req.body.password) throw new Error('Passwords do not match');
     return true;
